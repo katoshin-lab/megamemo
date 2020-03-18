439f710edfb9cb0603fe96d821c8b0443a4b416f
Kato Shinya <hatao0000000@outlook.com>
2020-03-19 02:39:43 +0900

    Edit color in signup dialog

--- src/components/Dialog/Signup.vue
+++ src/components/Dialog/Signup.vue
@@ -4,7 +4,7 @@
       align="center"
       justify="center"
     >
-      <div class="dialog">You required to sign in!</div>
+      <div class="dialog" color="red">You required to sign in!</div>
       <v-btn @click="signIn">
         <v-icon class="dialog__google">mdi-google</v-icon>
         Access Your Google Account
@@ -26,7 +26,6 @@ export default {
 <style lang="scss" scoped>
   .dialog {
     padding: 15px;
-    color: red;
     font-weight: bold;
     &__google {
       padding-right: 10px;
