90c4bd4d4e75d7683fb44a48bcc21cacf4579bec
Kato Shinya <hatao0000000@outlook.com>
2020-03-21 03:24:43 +0900

    remove firebaseConfig from main.js

--- src/main.js
+++ src/main.js
@@ -5,21 +5,11 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import firebase from 'firebase/app';
+import { firebaseConfig } from './plugins/firebaseConfig'
 import 'firebase/auth';
 import 'firebase/firestore';
 
 Vue.config.productionTip = false
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA9m7QYlzL105qJpyKCtwiX5NX623aGJdw",
-  authDomain: "megamemo-1420f.firebaseapp.com",
-  databaseURL: "https://megamemo-1420f.firebaseio.com",
-  projectId: "megamemo-1420f",
-  storageBucket: "megamemo-1420f.appspot.com",
-  messagingSenderId: "836383038964",
-  appId: "1:836383038964:web:f15303d87c079f58176da1",
-  measurementId: "G-ZLE723HZ0B"
-};
 
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore()
