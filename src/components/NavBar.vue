1cb644c524c5e83705fddf795e77b3cd3a251ecf
Kato Shinya <hatao0000000@outlook.com>
2020-03-22 19:15:02 +0900

    Update NavBar
    ナビゲーションバーでgridを使用

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,17 @@
     <v-system-bar
       height="30px"
     >
-      <span>Category: {{ selectedCategory }}</span>
+      <v-container>
+        <v-row>
+          <v-col>
+            <span>Category: {{ selectedCategory }}</span>
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col>
+            <span></span>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-system-bar>
   </div>
 </template>
